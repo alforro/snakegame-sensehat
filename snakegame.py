57d601044ad09b47e75c04d68a38a3c00d7508d7
Alvaro Rodriguez <alfa.alvaro.rodriguez@gmail.com>
2016-10-26 17:29:12 -0300

    the snake is only one led, it can move to any direction now

--- snakegame.py
+++ snakegame.py
@@ -4,11 +4,18 @@ import time
 from sense_hat import SenseHat
 from evdev import InputDevice, list_devices, ecodes
 
+
+
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+from signal import pause
+
 print("Press Ctrl-C to quit")
 time.sleep(1)
 
+x = 3
+y = 3
 sense = SenseHat()
-sense.clear()  # Blank the LED matrix
+sense.clear()
 
 found = False;
 devices = [InputDevice(fn) for fn in list_devices()]
@@ -20,42 +27,37 @@ if not(found):
   print('Raspberry Pi Sense HAT Joystick not found. Aborting ...')
   exit()
 
-# 0, 0 = Top left
-# 7, 7 = Bottom right
-UP_PIXELS = [[3, 0], [4, 0]]
-DOWN_PIXELS = [[3, 7], [4, 7]]
-LEFT_PIXELS = [[0, 3], [0, 4]]
-RIGHT_PIXELS = [[7, 3], [7, 4]]
-CENTRE_PIXELS = [[3, 3], [4, 3], [3, 4], [4, 4]]
+def clamp(value, min_value=0, max_value=7):
+    return min(max_value, max(min_value, value))
 
+def pushed_up(event):
+    global y
+    if event.action != ACTION_RELEASED:
+        y = clamp(y - 1)
 
-def set_pixels(pixels, col):
-    for p in pixels:
-        sense.set_pixel(p[0], p[1], col[0], col[1], col[2])
+def pushed_down(event):
+    global y
+    if event.action != ACTION_RELEASED:
+        y = clamp(y + 1)
 
+def pushed_left(event):
+    global x
+    if event.action != ACTION_RELEASED:
+        x = clamp(x - 1)
 
-def handle_code(code, colour):
-    if code == ecodes.KEY_DOWN:
-        set_pixels(DOWN_PIXELS, [255,0,0])
-    elif code == ecodes.KEY_UP:
-        set_pixels(UP_PIXELS, [0,255,0])
-    elif code == ecodes.KEY_LEFT:
-        set_pixels(LEFT_PIXELS, [0,0,255])
-    elif code == ecodes.KEY_RIGHT:
-        set_pixels(RIGHT_PIXELS, [255,0,255])
-    elif code == ecodes.KEY_ENTER:
-        set_pixels(CENTRE_PIXELS, [255,255,0])
+def pushed_right(event):
+    global x
+    if event.action != ACTION_RELEASED:
+        x = clamp(x + 1)
 
+def refresh():
+    sense.clear()
+    sense.set_pixel(x, y, 255, 255, 255)
 
-BLACK = [0, 0, 0]
-WHITE = [255, 255, 255]
-
-try:
-    for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY:
-            if event.value == 1:  # key down
-                handle_code(event.code, WHITE)
-            if event.value == 0:  # key up
-                handle_code(event.code, BLACK)
-except KeyboardInterrupt:
-    sys.exit()
+sense.stick.direction_up = pushed_up
+sense.stick.direction_down = pushed_down
+sense.stick.direction_left = pushed_left
+sense.stick.direction_right = pushed_right
+sense.stick.direction_any = refresh
+refresh()
+pause()
